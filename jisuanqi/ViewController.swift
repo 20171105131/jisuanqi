9145b9fdeaded08f94ee698d063d2fd477bae019
20171105131 <1105197998@qq.com>
2018-09-27 10:11:31 +0800

    计算器
    添加数字按键以及加号等号

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -9,7 +9,92 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var temp = 0
 
+    @IBOutlet weak var caculaterDisplay: UITextField!
+    
+    
+    @IBAction func buttonCaculater(_ sender: Any)
+    {
+        var sum = 0
+        sum = temp +
+            Int(caculaterDisplay.text!)!
+        caculaterDisplay.text = "\(sum)"
+    }
+    
+    
+    @IBAction func buttenClear(_ sender: Any)
+    {
+        caculaterDisplay.text = " "
+    }
+    
+    
+    @IBAction func buttonAnd(_ sender: Any)
+    {
+        
+    }
+    
+    
+    @IBAction func button1(_ sender: Any)
+    {
+        caculaterDisplay.text = "1"
+    }
+    
+    
+    @IBAction func button2(_ sender: Any)
+    {
+        caculaterDisplay.text = "2"
+    }
+    
+    
+    @IBAction func button3(_ sender: Any)
+    {
+        caculaterDisplay.text = "3"
+    }
+    
+    
+    @IBAction func button4(_ sender: Any)
+    {
+        caculaterDisplay.text = "4"
+    }
+    
+    
+    @IBAction func button5(_ sender: Any)
+    {
+        caculaterDisplay.text = "5"
+    }
+    
+    
+    @IBAction func button6(_ sender: Any)
+    {
+        caculaterDisplay.text = "6"
+    }
+    
+    
+    @IBAction func button7(_ sender: Any)
+    {
+        caculaterDisplay.text = "7"
+    }
+    
+    
+    @IBAction func button8(_ sender: Any)
+    {
+        caculaterDisplay.text = "8"
+    }
+    
+    
+    @IBAction func button9(_ sender: Any)
+    {
+        caculaterDisplay.text = "9"
+    }
+    
+    
+    @IBAction func button0(_ sender: Any)
+    {
+        caculaterDisplay.text = "0"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

ecf89af49a736e2744971600019301d60106c043
20171105131 <1105197998@qq.com>
2018-10-11 08:35:51 +0800

    增加数字按键以及修改问题

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -25,73 +25,75 @@ class ViewController: UIViewController {
     
     @IBAction func buttenClear(_ sender: Any)
     {
-        caculaterDisplay.text = " "
+        caculaterDisplay.text = ""
     }
     
     
     @IBAction func buttonAnd(_ sender: Any)
     {
-        
+        temp =
+        Int(caculaterDisplay.text!)!
+        caculaterDisplay.text=""
     }
     
     
     @IBAction func button1(_ sender: Any)
     {
-        caculaterDisplay.text = "1"
+        caculaterDisplay.text = caculaterDisplay.text! + "1"
     }
     
     
     @IBAction func button2(_ sender: Any)
     {
-        caculaterDisplay.text = "2"
+        caculaterDisplay.text = caculaterDisplay.text! + "2"
     }
     
     
     @IBAction func button3(_ sender: Any)
     {
-        caculaterDisplay.text = "3"
+        caculaterDisplay.text = caculaterDisplay.text! + "3"
     }
     
     
     @IBAction func button4(_ sender: Any)
     {
-        caculaterDisplay.text = "4"
+        caculaterDisplay.text = caculaterDisplay.text! + "4"
     }
     
     
     @IBAction func button5(_ sender: Any)
     {
-        caculaterDisplay.text = "5"
+        caculaterDisplay.text = caculaterDisplay.text! + "5"
     }
     
     
     @IBAction func button6(_ sender: Any)
     {
-        caculaterDisplay.text = "6"
+        caculaterDisplay.text = caculaterDisplay.text! + "6"
     }
     
     
     @IBAction func button7(_ sender: Any)
     {
-        caculaterDisplay.text = "7"
+        caculaterDisplay.text = caculaterDisplay.text! + "7"
     }
     
     
     @IBAction func button8(_ sender: Any)
     {
-        caculaterDisplay.text = "8"
+        caculaterDisplay.text = caculaterDisplay.text! + "8"
     }
     
     
     @IBAction func button9(_ sender: Any)
     {
-        caculaterDisplay.text = "9"
+        caculaterDisplay.text = caculaterDisplay.text! + "9"
     }
     
     
     @IBAction func button0(_ sender: Any)
     {
-        caculaterDisplay.text = "0"
+        caculaterDisplay.text = caculaterDisplay.text! + "0"
     }
     
     
